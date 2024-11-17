bf9f9e337c079dfc19afb78125cd5a83c57dd00c
adora0 <dorazioand@gmail.com>
2024-11-17 22:34:47 +0100

    Modifica inserimento dati

--- app.js
+++ app.js
@@ -85,21 +85,38 @@ app.post('/elab', (req, res) => {
 });
 
 /*endpoint /datidomanda
-Genera valori casuali per una serie storica
+Genera una serie storica di valori casuali per i componenti del calcolo
 Viene utilizzata la funzione javascript Math.random() che genera un valore decimale compreso tra 0 e 1.
+Valore restituito:
+ - dati, oggetto json con gli array dei valori casuali in serie storica di Costo unitario, Costo setup, costo mantenimento, Domanda
 */
 app.post('/datidomanda',(req,res) => {   
-     /*var periodi = Array.from({ length: 20 }, (_, i) => i + 1);*/
-    /*genero un valore di base random tra 100 e 100.000 
-      rappresenta la dimensione base della domanda*/
-    let base = Math.round(Math.random() * (100000 - 1000) + 1000);    
+    /*genero valori di base random per le varie componenti di calcolo*/
+    log('datidomanda','INFO');
+    let dati = [];
+    let maxVariazione = 0.1;    
+    let valD = Math.round(Math.random() * (100000 - 1000) + 1000);  
+    let valC = Math.round(Math.random() * (1001 - 1) + 1); 
+    let valS = Math.round(Math.random() * (1000 - 100) + 100);    
+    let valH = Math.round(Math.random() * valC); 
     /*genero un array di indici tra -1 e 1 che rappresentano la variazione della domanda in serie storica*/
-    var indici = Array.from({length: 20}, (_,i) => Math.random() * (Math.random() < 0.5 ? -1 : 1));
+    let indici = Array.from({length: 3}, (_,i) => Math.random() * (Math.random() < 0.5 ? -1 : 1));
     /*genero l'array dei dati in serie storica con una variazione della base al massimo di un 10%*/ 
-    let dati= indici.map(elem => Math.round(base * (10 -  elem) / 10));
+    for(let i=0; i<indici.length; i++){
+        valD += Math.floor((Math.random() * 2 - 1) * maxVariazione);
+        valC += Math.floor((Math.random() * 2 - 1) * maxVariazione);
+        valH += Math.floor((Math.random() * 2 - 1) * maxVariazione);
+        valS += Math.floor((Math.random() * 2 - 1) * maxVariazione);       
+        dati.push({
+            valD: valD, 
+            valC: valC, 
+            valH: valH, 
+            valS: valS
+        });
+    }   
     res.send(dati);
 });
-
+ 
 /*inizio ascolto del server su porta 3000*/
 app.listen(port, () => {
     log(`Server web in ascolto sulla porta:${port}`,'INFO');
--- public/js/script.js
+++ public/js/script.js
@@ -21,7 +21,7 @@ async function getEOQ() {
             document.getElementById('result').innerHTML = data;
         });
     } catch (error) {
-         showAlert('Errore', 'Errore durante il calcolo EOQ:' + error);
+        showAlert('Errore', 'Errore durante il calcolo EOQ:' + error);
     }
 }
 
@@ -41,8 +41,8 @@ async function showParams() {
             //Aggiorna la parte della pagina con il nuovo contenuto
             document.getElementById('main').innerHTML = data;
         });
-    } catch (error) {        
-         showAlert('Errore','Errore durante il richiamo del form parametri' + error);
+    } catch (error) {
+        showAlert('Errore', 'Errore durante il richiamo del form parametri' + error);
     }
 }
 
@@ -50,46 +50,75 @@ async function showParams() {
    Funzione per la generazione di una serie storica casuale.
    Richiama l'endpoint /datidomanda per la generazione di una serie storica casuale.
 */
-async function generaDati() {    
+async function generaDati() {
     try {
         await fetch('/datidomanda', {
             method: "POST"
         }).then(function (response) {
             return response.text();
-        }).then(function (data) {           
-           
+        }).then(function (data) {
+
             //Aggiorna la porzione di pagina con il nuovo contenuto            
-            var tabella=document.getElementById('tabellaDati');
-            tabella.innerHTML='';   
-            let datiDomanda = JSON.parse(data);  
-            let i=1;    
-            addRow();        
-          /*  datiDomanda.forEach((val) => {
-                let row = document.createElement('tr');             
-                  let cell = document.createElement('td');
-                  cell.appendChild(document.createTextNode("P"+ i));
-                  cell.className="small";
-                  row.appendChild(cell);                           
-                  cell=document.createElement('td');
-                  cell.appendChild(document.createTextNode(val.toLocaleString('it-IT')));
-                  cell.classList.add("small", "text-right");
-                  row.appendChild(cell);  
+            var tabella = document.getElementById('tabellaDati');
+            tabella.innerHTML = '';
+            let datiDomanda = JSON.parse(data);
+            let i = 1;
+
+            datiDomanda.forEach((dati) => {
+                let row = document.createElement('tr');
+                let cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(i));
+                row.appendChild(cell);
+
+                cell= document.createElement('td');
+                cell.appendChild(document.createTextNode('P1'));
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(dati.valC.toLocaleString('it-IT')));
+                cell.classList.add("text-right");
+                row.appendChild(cell);
+
+                cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(dati.valS.toLocaleString('it-IT')));
+                cell.classList.add("text-right");
+                row.appendChild(cell);
+                
+                cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(dati.valH.toLocaleString('it-IT')));
+                cell.classList.add("text-right");
+                row.appendChild(cell);
+
+                cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(dati.valD.toLocaleString('it-IT')));
+                cell.classList.add("text-right");
+                row.appendChild(cell);
+                
+                row.appendChild(cell);
+
                 tabella.appendChild(row);
-                i=i+1;
-              });*/
+                i = i + 1;
+            });
         });
-    } catch (error) {             
-         showAlert('Errore','Errore durante la generazione di dati'+ error);
-    } 
+    } catch (error) {
+        showAlert('Errore', 'Errore durante la generazione di dati' + error);
+    }
 }
 
-
-function addRow(){
-    let tabella=document.getElementById('tabellaDati');
-    let id = tabella.rows.length;
+/*addRow
+  funzione per l'inserimento di una riga nella tabella dati, per l'inserimento manuale dei dati
+  Valore restituito:
+  - il riferimento all'oggetto riga appena inserito
+*/
+function addRow() {
+    /*prelevo il body della tabella dal DOM*/
+    let tabella = document.getElementById('tabellaDati');
+    /*imposto l'id della riga da inserire*/
+    let num_riga = tabella.rows.length;
     let row = tabella.insertRow();
-    
-    let cellRiga = row.insertCell(0).innerHTML=id+1;
+    /*imposto l'id della riga*/
+    row.id = 'R' + num_riga;
+    /*aggiungo le celle della tabella*/
+    let cellRiga = row.insertCell(0).innerHTML = num_riga + 1;
     let cellPeriodo = row.insertCell(1);
     let cellCosto = row.insertCell(2);
     let cellSetup = row.insertCell(3);
@@ -97,20 +126,31 @@ function addRow(){
     let cellDomanda = row.insertCell(5);
     let cellEOQ = row.insertCell(6);
     let cellRemove = row.insertCell(7);
-
+    /*imposto le celle come editabili*/
     cellPeriodo.contentEditable = "true";
     cellCosto.contentEditable = "true";
     cellSetup.contentEditable = "true";
     cellMantenimento.contentEditable = "true";
-    cellDomanda.contentEditable = "true";    
-    cellRemove.innerHTML="<button type='button' class='btn btn-outline-danger' onclick='removeRow(" + row.rowIndex+ ")'>X</button>";
+    cellDomanda.contentEditable = "true";
+    /*aggiungo il pulsante per la rimozione della riga*/
+    cellRemove.innerHTML = "<button type='button' class='btn btn-outline-danger' onclick='removeRow(this)'>X</button>";
+    /*restituisco la riga inserita*/
+    return row;
 }
 
-
-function removeRow(row){
-    let tabella=document.getElementById('tabellaDati');
-    tabella.deleteRow(row.rowIndex);
+/*removeRow
+  funzione per la rimozione di una riga dalla tabella dati
+  Parametri:
+  - btn, oggetto del button che è stato cliccato*/
+function removeRow(btn) {
+    /*prelevo il body della tabella dal DOM*/
+    let tabella = document.getElementById('tabellaDati');
+    /*individuo la riga del button premuto*/
+    let row = btn.closest('tr');
+    /*rimuovo la riga dalla tabella*/
+    tabella.removeChild(row);
 }
+
 /*showButton 
     Funzione per la visualizazione dinamica del form inserimento dati, dati dell domanda D, se generati 
     automaticamente o inseriti tramite file csv.
@@ -119,12 +159,14 @@ function removeRow(row){
 */
 function showButton(btnID) {
     document.getElementById(btnID).className = "visible";
-    (btnID != "btnFileDati") ? document.getElementById("btnFileDati").className = "invisible" 
-        : document.getElementById("btnFileDati").className = "visible" ;
+    (btnID != "btnFileDati") ? document.getElementById("btnFileDati").className = "invisible"
+        : document.getElementById("btnFileDati").className = "visible";
     (btnID != "btnDatiRandom") ? document.getElementById("btnDatiRandom").className = "invisible"
-        : document.getElementById("btnDatiRandom").className = "visible" ;
+        : document.getElementById("btnDatiRandom").className = "visible";
     (btnID != "btnDatiInserimento") ? document.getElementById("btnDatiInserimento").className = "invisible"
-        : document.getElementById("btnDatiInserimento").className = "visible" ;
+        : document.getElementById("btnDatiInserimento").className = "visible";
+    /*inizializzo la tabella dati*/
+    document.getElementById('tabellaDati').innerHTML="";
 }
 
 /*showAlert
@@ -133,10 +175,10 @@ function showButton(btnID) {
     - titolo, titolo dell'alert;
     - messaggio, corpo del messaggio di alert.
 */
-function showAlert(titolo, messaggio){
-    var modal = new bootstrap.Modal(document.getElementById('alert')); 
+function showAlert(titolo, messaggio) {
+    var modal = new bootstrap.Modal(document.getElementById('alert'));
     modal.show();
-    document.getElementById("alertTitolo").innerHTML=titolo;
-    document.getElementById("alertMessaggio").innerHTML=messaggio;
+    document.getElementById("alertTitolo").innerHTML = titolo;
+    document.getElementById("alertMessaggio").innerHTML = messaggio;
 }
 
